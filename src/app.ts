91d99d77a9019908b689e66a3a56468eb6c81d02
antonpalermo <anpbusinessmail@gmail.com>
2023-09-03 22:20:10 +0800

    initialize busboy

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,22 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 import express, { Application, Router } from "express";
 
 import productsRoute from "./routers/products";
 
-dotenv.config()
+dotenv.config();
 
 async function main() {
+  // express application
   const app: Application = express();
+  // server port
   const port = process.env.PORT || 5000;
   // base route instance
   const baseRouter: Router = express.Router();
-
   // products routes
   baseRouter.use("/products", productsRoute);
-
+  // use to parse url encoded requests.
+  app.use(express.urlencoded({ extended: true }));
+  // consume base route
   app.use("/api", baseRouter);
 
   app.listen(port, () => {
--- src/routers/products.ts
+++ src/routers/products.ts
@@ -1,11 +1,30 @@
+import Busboy from "busboy";
 import express, { Request, Response, Router } from "express";
 
 const router: Router = express.Router();
 
 router.post("/:storeId/upload", (req: Request, res: Response) => {
-  const storeId = req.query.storeId;
+  const busboy = Busboy({ headers: req.headers });
 
-  return res.status(201).json({ message: `Image uploaded successfully on ${storeId}` });
+  busboy.on("file", (name, file, info) => {
+    console.log("sample", name, file, info);
+
+    file.on("data", (chunk) => {
+      console.log("chunk", chunk);
+    });
+
+    file.on("close", () => {
+      console.log(`file ${name} done`);
+    });
+  });
+
+  busboy.on('close', () => {
+    console.log('done parsing form')
+  })
+
+  req.pipe(busboy)
+
+  return res.status(201).json({ message: "okay" });
 });
 
 export default router;
