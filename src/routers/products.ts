7ce972070486b2ecf00da646f8a31c13163acc50
antonpalermo <anpbusinessmail@gmail.com>
2023-09-11 21:15:32 +0800

    feat: generate presigned urls
    users can now view images stored in the s3 bucket using a presigned url.

--- src/routers/products.ts
+++ src/routers/products.ts
@@ -2,9 +2,12 @@ import multer from "multer";
 import express, { Request, Response, Router } from "express";
 import {
   S3Client,
+  GetObjectCommand,
   PutObjectCommand,
+  GetObjectCommandInput,
   PutObjectCommandInput,
 } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 import prisma from "../libs/prisma";
 import resizeImage from "../middlewares/resize-image";
@@ -23,6 +26,42 @@ const s3Client = new S3Client({
 });
 
 router.use(multer().single("image"));
+
+type Image = {
+  name: string;
+  url?: string;
+};
+
+router.get("/:storeId/images", async (req: Request, res: Response) => {
+  try {
+    // will only return array of image names.
+    const images: Image[] = await prisma.images.findMany({
+      select: { name: true },
+    });
+
+    try {
+      for (const image of images) {
+        const params: GetObjectCommandInput = {
+          Bucket: process.env.AWS_BUCKET_NAME,
+          Key: image.name,
+        };
+        const command = new GetObjectCommand(params);
+        const signedURL = await getSignedUrl(s3Client, command, {
+          expiresIn: 3600,
+        });
+        image.url = signedURL;
+      }
+
+      return res.status(200).json(images);
+    } catch (e) {
+      return res
+        .status(500)
+        .json({ message: "unable to generate signed urls" });
+    }
+  } catch (e) {
+    return res.status(500).json({ message: "unable to get all images!" });
+  }
+});
 
 router.post(
   "/:storeId/upload",
